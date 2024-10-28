732cfdb0c4a021c144c89609beff8f3ba2343ed6
Elina Anna Schröder <elina-schroeder@freenet.de>
2024-10-28 19:54:53 +0100

    World done

--- Assets/Scripts/Item_Data.cs
+++ Assets/Scripts/Item_Data.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Script : MonoBehaviour
+public class Item_Data : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
